430e3dd6cf9a842219610a1ad0a0df4d9209a9a9
Matthias Schmidt <matthias.schmidt.92@gmx.de>
2025-06-19 19:58:25 +0200

    Created a working cocktail ingredient relationship

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from .routers import ingredients, root, longdrinks, cocktails
+from .routers import ingredients, root, cocktails
 from .db import create_db_and_tables
 
 if TYPE_CHECKING:
@@ -19,9 +19,9 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[Any, Any]:
         router=ingredients.router, prefix="/ingredients", tags=["Ingredients"]
     )
     app.include_router(router=cocktails.router, prefix="/cocktails", tags=["Cocktails"])
-    app.include_router(
-        router=longdrinks.router, prefix="/longdrinks", tags=["Longdrinks"]
-    )
+    # app.include_router(
+    #    router=longdrinks.router, prefix="/longdrinks", tags=["Longdrinks"]
+    # )
 
     yield
 
--- src/models.py
+++ src/models.py
@@ -2,25 +2,6 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import List, Optional
 
 
-class Ingredient(SQLModel, table=True):
-    __tablename__ = "ingredients"
-    id: int = Field(primary_key=True)
-    name: str = Field(nullable=False)
-    is_available: bool = Field(nullable=False, default=True)
-
-
-class Cocktail(SQLModel, table=True):
-    __tablename__ = "cocktails"
-    id: int | None = Field(primary_key=True, nullable=False, default=None)
-    name: str = Field(nullable=False, unique=True)
-
-
-class Longdrink(SQLModel, table=True):
-    __tablename__ = "longdrinks"
-    id: int | None = Field(primary_key=True, nullable=False, default=None)
-    name: str = Field(nullable=False, unique=True)
-
-
 class CocktailIngredientLink(SQLModel, table=True):
     cocktail_id: Optional[int] = Field(
         default=None, foreign_key="cocktails.id", primary_key=True
@@ -31,19 +12,47 @@ class CocktailIngredientLink(SQLModel, table=True):
     amount_ml: float
     unit: str = "ml"
 
-    cocktail: Optional["Cocktail"] = Relationship(back_populates="ingredients")
-    ingredient: Optional["Ingredient"] = Relationship()
+    cocktail: Optional["Cocktail"] = Relationship(back_populates="ingredient_links")
+    ingredient: Optional["Ingredient"] = Relationship(back_populates="cocktail_links")
 
 
-class LondrinkIngredientLink(SQLModel, table=True):
-    longdrink_id: Optional[int] = Field(
-        default=None, foreign_key="longdrinks.id", primary_key=True
+class Cocktail(SQLModel, table=True):
+    __tablename__ = "cocktails"
+    id: int | None = Field(primary_key=True, nullable=False, default=None)
+    name: str = Field(nullable=False, unique=True)
+    ingredient_links: List[CocktailIngredientLink] = Relationship(
+        back_populates="cocktail"
     )
-    ingredient_id: Optional[int] = Field(
-        default=None, foreign_key="ingredients.id", primary_key=True
-    )
-    amount_ml: float
-    unit: str = "ml"
 
-    longdrink: Optional["Longdrink"] = Relationship(back_populates="ingredients")
-    ingredient: Optional["Ingredient"] = Relationship()
+
+class Ingredient(SQLModel, table=True):
+    __tablename__ = "ingredients"
+    id: int = Field(primary_key=True)
+    name: str = Field(nullable=False)
+    is_available: bool = Field(nullable=False, default=True)
+    cocktail_links: List[CocktailIngredientLink] = Relationship(
+        back_populates="ingredient"
+    )
+
+
+# class LongdrinkIngredientLink(SQLModel, table=True):
+#    longdrink_id: Optional[int] = Field(
+#        default=None, foreign_key="longdrinks.id", primary_key=True
+#    )
+#    ingredient_id: Optional[int] = Field(
+#        default=None, foreign_key="ingredients.id", primary_key=True
+#    )
+#    amount_ml: float
+#    unit: str = "ml"
+#
+#    longdrink: Optional["Longdrink"] = Relationship(back_populates="ingredient_links")
+#    ingredient: Optional["Ingredient"] = Relationship()
+#
+#
+# class Longdrink(SQLModel, table=True):
+#    __tablename__ = "longdrinks"
+#    id: int | None = Field(primary_key=True, nullable=False, default=None)
+#    name: str = Field(nullable=False, unique=True)
+#    ingredient_link: List["Ingredient"] = Relationship(
+#        back_populates="longdrinks", link_model=LongdrinkIngredientLink
+#    )
